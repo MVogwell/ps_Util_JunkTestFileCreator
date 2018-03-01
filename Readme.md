db5f337c1209a6dab42b94d2a698a24ad5a46196
Martin Vogwell <36699951+MVogwell@users.noreply.github.com>
2018-03-01 10:14:11 +0000

    Powershell script - creates junk test files
    Powershell script to create junk test files at exact MB sizes

---
+++ Readme.md
@@ -0,0 +1,63 @@
+## ps_Util_JunkTestFileCreator
+
+MVogwell - 01-03-18 - v1.0
+
+## Synopsis
+
+This is a powershell script to create a text file of a specific MB size. 
+
+Parameters:
+
+-JunkFileSizeMB	= Optional. Integer. The size of the junk file to create in MB
+-JunkFileName	= Optional. String. Set a custom file name (otherwise JunkTestFile_<size>MB.txt will be used)
+	
+## Motivation
+
+I'm using this to create custom sized files for network bandwidth testing over SMB
+
+
+## Installation / Running the function
+
+	This script does not need installing but needs to be run from a folder that is writable for the user running it.	
+
+	Open Powershell and run the script. It may be necessary to change the Powershell ExeceutionPolicy. See https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/set-executionpolicy?view=powershell-6
+
+	The junk file will be created in the same folder as the script is located.
+
+	The JunkFileSizeMB parameter should be entered as an integer
+
+	The JunkFileName should be a file name (e.g. Junk.txt)
+
+## Examples
+	.\ps_Util_JunkTestFileCreator.ps1
+		This will run the script which will then ask for the file size. The output will be located in the same folder as the script and named JunkTestFile_<size>MB.txt
+
+	.\ps_Util_JunkTestFileCreator.ps1 -JunkFileSizeMB 1
+		This will create a file 1MB in size called JunkTestFile_1MB.txt in the same folder as the script.
+
+	.\ps_Util_JunkTestFileCreator.ps1 -JunkFileSizeMB 1 -JunkFileName Junk.txt
+		This will create a file 1MB in size called Junk.txt in the same folder as the script.
+	
+## License
+	Free to use, change, and distribute. 
+
+## Disclaimer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
---
+++ ps_Util_JunkTestFileCreator.ps1
@@ -0,0 +1,133 @@
+#########################################
+#
+# Generate junk test files
+#
+# MVogwell - March 2018
+#
+#
+
+[CmdletBinding()]
+param (
+	[Parameter(Mandatory=$false)][int]$JunkFileSizeMB = 0,
+	[Parameter(Mandatory=$false)][string]$JunkFileName = ""
+)
+
+$ErrorActionPreference = "Stop"
+
+Function Test-StringIsValidFileName() {
+	param (
+		[Parameter(Mandatory=$true)][string]$sFileName	
+	)
+	
+	# Check the string doesn't contain characters which are invalid in a Windows file name.
+	if($sFileName -match '\\|\"|\/|\?|\:|\||\<|\>') {
+		return $False	# Not a valid file name
+	}
+	Else {
+		return $True	# A valid file name
+	}
+}
+
+[int]$iJunkFileSize = $JunkFileSizeMB
+
+write-host ""
+write-host ""
+write-host "Junk file creator - can be used for baseline network copy tests" -fore green
+
+write-host ""
+
+# If the size 
+if($iJunkFileSize -eq 0) {
+	Try {
+		[int]$iJunkFileSize = read-host "Enter the size of the junk file (as a number in MB)"
+	}
+	Catch [ArgumentTransformationMetadataException] {
+		write-host "That's not a number!! Try again"
+		write-host
+		write-output $Error[0] | select -property * | fl	
+	}
+	Catch {
+		write-host "Something bad happened! Quiting..."
+		write-host
+		write-output $Error[0] | select -property * | fl
+	}
+	Finally {}
+}
+
+if(($iJunkFileSize -lt 1) -or ($iJunkFileSize -gt 3027)) {
+	write-host "Unable to create the file. The Junk File Size much be between 1 and 3027"
+}
+Else {
+	$iJunkFileSizeBytes = $iJunkFileSize * 1024 * 1024 # Convert the size from MB to bytes
+
+	# Set the file path
+		
+	if($JunkFileName.length -eq 0) {
+		$sDestFile = $PSScriptRoot + "\JunkTestFile_" + $iJunkFileSize + "MB.txt"
+	}
+	Else {
+		$bFileNameValid = Test-StringIsValidFileName -sFileName $JunkFileName
+		if($bFileNameValid){ 
+			$sDestFile = $PSScriptRoot + "\" + $JunkFileName
+		}
+		else {
+			write-host 'You have an entered an invalid file name. File names cannot contain the characters " \ / | < > ? :' -fore red -back white
+			write-host "Please try again..." -fore red -back white
+			write-host ""
+		}
+	}
+
+	if($bFileNameValid) {
+		# Make sure the file can be created before continuing
+		Try {
+			$bFileValid = $True
+			New-Item $sDestFile -ItemType "file" -Force | out-null
+		}
+		Catch {
+			$bFileValid = $False
+			write-host "It has not been possible to create the junk file" -fore red -back white
+			write-host "Check the following path is writable:" -fore red -back white
+			write-host $sDestFile -fore red -back white
+			write-host ""
+			write-output $Error[0] | select -property * | fl
+			write-host ""
+			write-host ""
+		}
+		Finally {}
+		
+		# If the file path is valid then generate the contents of the file
+		Try {
+			if($bFileValid) {
+				
+				write-host "Creating file $sDestFile at $iJunkFileSize MB" -fore yellow			
+				write-host "Start time: $(get-date)"
+				
+				$stream = [System.IO.StreamWriter]::new( $sDestFile )
+			
+				Do {
+					$stream.writeline("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111	1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111	1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111	111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111")
+					}
+				While ((get-item $sDestFile).length -lt $iJunkFileSizeBytes )
+				
+				write-host ""
+				write-host ""
+				write-host "End time: $(get-date)"
+				write-host "Complete" -fore green
+				
+				
+				write-host ""
+				write-host ""	
+			}
+		}
+		Catch {
+			write-host ""
+			write-host "It has not been possible to create the file: "
+			write-host ""
+			write-output $Error[0] | select -property * | fl
+		}
+		Finally {
+			$stream.close()
+		}
+
+	}
+}
